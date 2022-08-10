f8f83b012aed1fae0e00f6c3c58d4b26a7234abf
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-08-10 16:07:13 -0400

    style: p6ify

--- init.zsh
+++ init.zsh
@@ -1,4 +1,4 @@
-
+# shellcheck shell=bash
 ######################################################################
 #<
 #
@@ -7,9 +7,9 @@
 #>
 ######################################################################
 p6df::modules::p6perl::deps() {
-    ModuleDeps=(
-      p6m7g8-dotfiles/p6common
-    )
+  ModuleDeps=(
+    p6m7g8-dotfiles/p6common
+  )
 }
 
 ######################################################################
@@ -22,7 +22,9 @@ p6df::modules::p6perl::deps() {
 ######################################################################
 p6df::modules::p6perl::init() {
 
-    p6_perl_init $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6perl
+  p6_perl_init $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6perl
+
+  p6_return_void
 }
 
 ######################################################################
@@ -40,5 +42,7 @@ p6_perl_init() {
     local dir="$1"
 
     p6_path_if "$dir/bin"
-    export PERL5LIB=$dir/lib/perl5
+    p6_env_export PERL5LIB "$dir/lib/perl5"
+
+    p6_return_void
 }
