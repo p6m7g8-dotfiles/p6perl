07fbe7762cd22520b6521716b6f6a3db58748c4a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-03-25 19:28:15 -0400

    chore: convert readme to template toolkit; fixes markdown lint (#6)

--- README.md
+++ README.md
@@ -1,96 +1,94 @@
-# p6perl
+# P6's POSIX.2: p6perl
 
 ## Table of Contents
 
-
-### p6perl
-- [p6perl](#p6perl)
-  - [Badges](#badges)
-  - [Distributions](#distributions)
-  - [Summary](#summary)
-  - [Contributing](#contributing)
-  - [Code of Conduct](#code-of-conduct)
-  - [Usage](#usage)
-  - [Author](#author)
-
-### Badges
+## Badges
 
 [![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)
-[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/p6m7g8/p6perl)
-[![Mergify](https://img.shields.io/endpoint.svg?url=https://gh.mergify.io/badges/p6m7g8/p6perl/&style=flat)](https://mergify.io)
-[![codecov](https://codecov.io/gh/p6m7g8/p6perl/branch/master/graph/badge.svg?token=14Yj1fZbew)](https://codecov.io/gh/p6m7g8/p6perl)
-[![Gihub repo dependents](https://badgen.net/github/dependents-repo/p6m7g8/p6perl)](https://github.com/p6m7g8/p6perl/network/dependents?dependent_type=REPOSITORY)
-[![Gihub package dependents](https://badgen.net/github/dependents-pkg/p6m7g8/p6perl)](https://github.com/p6m7g8/p6perl/network/dependents?dependent_type=PACKAGE)
+[![Mergify](https://img.shields.io/endpoint.svg?url=https://gh.mergify.io/badges//p6perl/&style=flat)](https://mergify.io)
+[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](<https://gitpod.io/#https://github.com//p6perl>)
 
 ## Summary
 
 ## Contributing
 
-- [How to Contribute](CONTRIBUTING.md)
+- [How to Contribute](<https://github.com//.github/blob/main/CONTRIBUTING.md>)
 
 ## Code of Conduct
 
-- [Code of Conduct](https://github.com/p6m7g8/.github/blob/master/CODE_OF_CONDUCT.md)
+- [Code of Conduct](<https://github.com//.github/blob/main/CODE_OF_CONDUCT.md>)
 
 ## Usage
 
-
 ### Aliases
-
 
 ### Functions
 
-### p6perl:
+## p6perl
 
-#### p6perl/init.zsh:
+### p6perl/init.zsh
 
-- p6_perl_init(dir)
 - p6df::modules::p6perl::deps()
 - p6df::modules::p6perl::init(_module, dir)
+- p6_perl_init(dir)
 
+## Hierarchy
 
-
-## Hier
 ```text
 .
-└── perl5
-    ├── P6
-    │   ├── AWS
-    │   │   ├── EC2
-    │   │   │   └── VPC
-    │   │   │       └── SG.pm
-    │   │   ├── ELB.pm
-    │   │   ├── SGen
-    │   │   │   ├── Cmd.pm
-    │   │   │   └── Service.pm
-    │   │   └── SGen.pm
-    │   ├── CICD
-    │   │   └── Docs
-    │   │       ├── Inline.pm
-    │   │       └── Readme.pm
-    │   ├── CLI.pm
-    │   ├── Cache.pm
-    │   ├── Cmd.pm
-    │   ├── Const.pm
-    │   ├── DB.pm
-    │   ├── DT.pm
-    │   ├── Email.pm
-    │   ├── IO.pm
-    │   ├── MVC
-    │   │   ├── C
-    │   │   │   └── Router.pm
-    │   │   ├── C.pm
-    │   │   ├── M.pm
-    │   │   ├── Util
-    │   │   │   └── DB.pm
-    │   │   └── V.pm
-    │   ├── Object.pm
-    │   ├── Template.pm
-    │   └── Util.pm
-    └── P6.pm
+├── README.md
+├── bin
+│   ├── doc_inline.pl
+│   ├── doc_readme.pl
+│   ├── elb_listener_show.pl
+│   ├── gen.pl
+│   └── sg_show.pl
+├── init.zsh
+├── lib
+│   └── perl5
+│       ├── P6
+│       │   ├── AWS
+│       │   │   ├── EC2
+│       │   │   │   └── VPC
+│       │   │   │       └── SG.pm
+│       │   │   ├── ELB.pm
+│       │   │   ├── SGen
+│       │   │   │   ├── Cmd.pm
+│       │   │   │   └── Service.pm
+│       │   │   └── SGen.pm
+│       │   ├── CICD
+│       │   │   └── Docs
+│       │   │       ├── Inline.pm
+│       │   │       └── Readme.pm
+│       │   ├── CLI.pm
+│       │   ├── Cache.pm
+│       │   ├── Cmd.pm
+│       │   ├── Const.pm
+│       │   ├── DB.pm
+│       │   ├── DT.pm
+│       │   ├── Email.pm
+│       │   ├── IO.pm
+│       │   ├── MVC
+│       │   │   ├── C
+│       │   │   │   └── Router.pm
+│       │   │   ├── C.pm
+│       │   │   ├── M.pm
+│       │   │   ├── Util
+│       │   │   │   └── DB.pm
+│       │   │   └── V.pm
+│       │   ├── Object.pm
+│       │   ├── Template.pm
+│       │   └── Util.pm
+│       └── P6.pm
+└── tt
+    ├── aws_func.tt
+    ├── aws_uw_func.tt
+    └── readme
+        └── readme.md.tt
 
-12 directories, 24 files
+16 directories, 34 files
 ```
+
 ## Author
 
 Philip M . Gollucci <pgollucci@p6m7g8.com>
--- lib/perl5/P6/CICD/Docs/Readme.pm
+++ lib/perl5/P6/CICD/Docs/Readme.pm
@@ -14,8 +14,9 @@ use File::Basename ();
 ## Globals
 
 ## SDK
-use P6::IO   ();
-use P6::Util ();
+use P6::IO       ();
+use P6::Template ();
+use P6::Util     ();
 
 ## Constants
 
@@ -41,114 +42,6 @@ sub _post_init {
     return;
 }
 
-sub readme_gen() {
-    my $self = shift;
-    my %args = @_;
-
-    my $omodule = $self->module();
-    my $module  = File::Basename::basename($omodule);
-
-    print "# $module\n\n";
-
-    print "## Table of Contents\n\n";
-    print "
-### $module
-- [$module](#$module)
-  - [Badges](#badges)
-  - [Distributions](#distributions)
-  - [Summary](#summary)
-  - [Contributing](#contributing)
-  - [Code of Conduct](#code-of-conduct)
-  - [Usage](#usage)
-  - [Author](#author)
-
-### Badges
-
-[![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)
-[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/p6m7g8/$module)
-[![Mergify](https://img.shields.io/endpoint.svg?url=https://gh.mergify.io/badges/p6m7g8/$module/&style=flat)](https://mergify.io)
-[![codecov](https://codecov.io/gh/p6m7g8/$module/branch/master/graph/badge.svg?token=14Yj1fZbew)](https://codecov.io/gh/p6m7g8/$module)
-[![Gihub repo dependents](https://badgen.net/github/dependents-repo/p6m7g8/$module)](https://github.com/p6m7g8/$module/network/dependents?dependent_type=REPOSITORY)
-[![Gihub package dependents](https://badgen.net/github/dependents-pkg/p6m7g8/$module)](https://github.com/p6m7g8/$module/network/dependents?dependent_type=PACKAGE)
-
-## Summary
-
-## Contributing
-
-- [How to Contribute](CONTRIBUTING.md)
-
-## Code of Conduct
-
-- [Code of Conduct](https://github.com/p6m7g8/.github/blob/master/CODE_OF_CONDUCT.md)
-
-## Usage
-
-";
-
-    print "
-### Aliases
-
-";
-
-    my $aliases = $self->aliases();
-    foreach my $from ( sort keys %$aliases ) {
-        my $to = $aliases->{$from};
-        print "- $from -> $to\n";
-    }
-
-    print "
-### Functions
-
-";
-
-    my $funcs = $self->funcs();
-
-    foreach my $dir ( sort keys %$funcs ) {
-        my $dir_funcs = $funcs->{$dir};
-
-        foreach my $subdir ( sort keys %$dir_funcs ) {
-            my $file_funcs = $dir_funcs->{$subdir};
-
-            $subdir ||= $module;
-            print "### $subdir:\n\n";
-
-            foreach my $file ( sort keys %$file_funcs ) {
-                next if $file =~ /debug/;
-                next if $file =~ /\/_/;
-                my @funcs = sort @{ $file_funcs->{$file} };
-
-                my $pfile = $file;
-                $file =~ s/^.*?$subdir/$subdir/;
-                print "#### $file:\n\n";
-
-                foreach my $func (@funcs) {
-                    next if $func =~ /__/;
-                    print "- $func\n";
-                }
-
-                print "\n";
-            }
-
-            print "\n";
-        }
-
-        print "\n";
-    }
-
-    if ( -d "$omodule/lib" ) {
-        print "## Hier\n";
-        print "```text\n";
-        system "(cd $omodule/lib ; tree)";
-        print "```\n";
-    }
-
-    print "## Author
-
-Philip M . Gollucci <pgollucci\@p6m7g8.com>
-";
-    return;
-}
-
 sub children() {
     my $self = shift;
     my $dir  = shift;
@@ -161,60 +54,38 @@ sub children() {
     $children;
 }
 
-sub dirs() {
-    my $self = shift;
-
-    my $module_dir = $self->module();
-    my $lib_dir    = "$module_dir/lib";
-    my $dirs       = [$lib_dir];
-
-    P6::Util::debug("module_dir: $module_dir\n");
-    P6::Util::debug("lib_dir: $lib_dir\n");
-    P6::Util::debug_dumper( "DIRS", $dirs );
-
-    $dirs;
-}
-
 sub parse {
     my $self = shift;
 
     my $funcs      = {};
     my $aliases    = {};
     my $module_dir = $self->module();
-    my $dirs       = $self->dirs();
 
     if ( -e "$module_dir/init.zsh" ) {
         my $lines = P6::IO::dread("$module_dir/init.zsh");
 
         foreach my $line (@$lines) {
-            if ( $line =~ /# Function: (.*)/ ) {
-                push
-                  @{ $funcs->{"$module_dir/lib"}->{""}->{"$module_dir/init.zsh"}
-                  }, $1;
-            }
-            elsif ( $line =~ /p6_alias "([^"]+)" ["']([^"]+)["']/ ) {
+            if ( $line =~ /p6_alias "([^"]+)" ["']([^"]+)["']/ ) {
                 my ( $from, $to ) = ( $1, $2 );
                 $aliases->{$from} = $to;
             }
         }
     }
 
-    foreach my $dir ( sort @$dirs ) {
-        my $children = $self->children($dir);
+    my $children = $self->children($module_dir);
 
-        foreach my $file ( sort @$children ) {
-            my $lines = P6::IO::dread($file);
+    foreach my $file ( sort @$children ) {
+        my $lines = P6::IO::dread($file);
 
-            foreach my $line (@$lines) {
-                if ( $line =~ /# Function: (.*)/ ) {
-                    my $func   = $1;
-                    my $subdir = File::Basename::dirname $file;
+        foreach my $line (@$lines) {
+            if ( $line =~ /# Function: (.*)/ ) {
+                my $func   = $1;
+                my $subdir = File::Basename::dirname $file;
 
-                    $subdir =~ s!$module_dir/lib/!!;
-                    P6::Util::debug("SUBDIR2: $subdir\n");
+                $subdir =~ s!$module_dir/lib/!!;
+                P6::Util::debug("SUBDIR2: $subdir\n");
 
-                    push @{ $funcs->{$dir}->{$subdir}->{$file} }, $func;
-                }
+                push @{ $funcs->{$subdir}->{$file} }, $func;
             }
         }
     }
@@ -225,6 +96,37 @@ sub parse {
     return;
 }
 
-sub tmpl_paths { "$ENV{PERL5LIB}/../../tt" }
+sub readme_gen() {
+    my $self = shift;
+    my %args = @_;
 
+    my $omodule = $self->module();
+    my $aliases = $self->aliases();
+    my $funcs   = $self->funcs();
+    my $path    = $self->tmpl();
+    my $module  = File::Basename::basename($omodule);
+
+    my $hier = qx/cd $module ; tree/;
+
+    my $data = {
+        aliases => $aliases,
+        funcs   => $funcs,
+        module  => $module,
+        hier    => $hier,
+    };
+
+    my $rv = P6::Template->render(
+        $data,
+        %args,
+        paths  => $self->tmpl_paths(),
+        ifile  => $self->tmpl(),
+        output => "$module/" . $self->output(),
+    );
+
+    return $rv;
+}
+
+sub output     { "README.md" }
+sub tmpl_paths { "$ENV{PERL5LIB}/../../tt" }
+sub tmpl       { "readme/readme.md.tt" }
 1;
